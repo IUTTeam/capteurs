13fd4caee627c01eb0601986b920af1317639959
lchateau <lucas.chateau@edu.univ-fcomte.fr>
2020-03-23 15:23:19 +0100

    enregistrement sur le serveur

--- communication_arduino.py
+++ communication_arduino.py
@@ -3,9 +3,12 @@ import serial.tools.list_ports
 import sys
 import time
 import datetime
+import requests
 import consts
 
 BAUDRATE = 9600
+
+# Programme de communication python arduino : ArduinoModule doit être dans l'arduino
 
 liste_port = []
 for port in serial.tools.list_ports.comports():
@@ -21,5 +24,12 @@ with serial.Serial(port=liste_port[0].device, baudrate=BAUDRATE) as arduino:
         message = consts.READ_ANALOG_TEMP
         arduino.write(message.encode("utf-8"))
         reponse = arduino.readline().decode("utf-8").splitlines()[0]
-        print(datetime.datetime.utcnow(), reponse)
+        data = {
+            "type": "temperature",
+            "donnees": [
+                [reponse, time.time()]
+            ]
+        }
+        r = requests.post(consts.URL_ENREGISTREMENT, json=data)
+        print(data, r.text)
         time.sleep(2)
--- consts.py
+++ consts.py
@@ -1,1 +1,2 @@
 READ_ANALOG_TEMP = "1"
+URL_ENREGISTREMENT = "https://service.frfr.duckdns.org/send_data"
