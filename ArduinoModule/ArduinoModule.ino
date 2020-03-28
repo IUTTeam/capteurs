9b06246499c2878b1d6e6cd165f742df53706d5f
lchateau <lucas.chateau@edu.univ-fcomte.fr>
2020-03-28 15:28:50 +0100

    Ajout capteur distance arduino

--- ArduinoModule/ArduinoModule.ino
+++ ArduinoModule/ArduinoModule.ino
@@ -1,14 +1,22 @@
 #include <math.h>
 
-int receivedMessage;
+#define analogSensorPin A5 // for KY-013 - Analog temperature
 
-int analogSensorPin = A5; // for KY-013 - Analog temperature
+#define trigPin 2
+#define echoPin 3
+
 
 const int READ_ANALOG_TEMP = 1;
+const int READ_DISTANCE = 2;
 
 const String START_MESSAGE = "Successfully initialised";
 
 void setup() {
+  // define inputs and outputs
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  // begin serial communications and parameters
   Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
   Serial.setTimeout(20);
   Serial.println(START_MESSAGE);
@@ -18,10 +26,13 @@ void loop() {
   // reply only when you receive data:
   if (Serial.available() > 0) {
     // read the incoming byte:
-    receivedMessage = Serial.parseInt();
+    int receivedMessage = Serial.parseInt();
     switch (receivedMessage) {
       case READ_ANALOG_TEMP:
         Serial.println(readAnalogTemperature());
+        break;
+      case READ_DISTANCE:
+        Serial.println(readDistance());
         break;
       default:
         Serial.println("ERROR");
@@ -39,4 +50,19 @@ double readAnalogTemperature() {
   temp = 1 / (((log(rt / 10000)) / 3950) + (1 / (273.15 + 25)));
   temp = temp - 273.15;
   return temp;
+}
+
+double readDistance() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  // Trigger the sensor by setting the trigPin high for 10 microseconds:
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Read the echoPin, pulseIn() returns the duration (length of the pulse) in microseconds:
+  long duration = pulseIn(echoPin, HIGH);
+  // Calculate the distance:
+  double distance = duration*0.034/2.;
+  return distance;
+  
 }
