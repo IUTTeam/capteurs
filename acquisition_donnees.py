b0eec03499bef368ad3bae4575dca635ad93a84d
lchateau <lucas.chateau@edu.univ-fcomte.fr>
2020-03-30 16:53:17 +0200

    Nettoyage code

--- acquisition_donnees.py
+++ acquisition_donnees.py
@@ -15,7 +15,7 @@ BAUDRATE = 9600
 
 app = Flask(__name__)
 
-liste_th = []
+threads_en_cours = []
 capteurs_libre = copy.deepcopy(consts.CAPTEURS)
 arduino = None
 mutex = threading.Lock()
@@ -31,66 +31,53 @@ def start():
     if len(liste_port) == 0:
         print("Erreur : Pas de port trouvé")
         sys.exit(1)
-
+    # connection à l'arduino par le port série
     arduino = serial.Serial(port=liste_port[0].device, baudrate=BAUDRATE)
     print(arduino.readline().decode("ascii").splitlines()[0])
 
     dao = mesureDAO.MesureDAO()
+
+    # création d'une première récolte de température
     th_temperature = communication.Communication(
-        arduino, 5, consts.CAPTEUR_TEMPERATURE, "temperature", mutex)
+        arduino, 5, consts.CAPTEUR_TEMPERATURE, "temperaturetest", mutex)
     th_temperature.start()
-    liste_th.append(th_temperature)
+    # ajout à la liste des acquisitions en cours
+    threads_en_cours.append(th_temperature)
     capteurs_libre.remove(consts.CAPTEUR_TEMPERATURE)
-    # th_distance = communication.Communication(
-    #     arduino, 5, "distancetest", "cm", consts.READ_DISTANCE, mutex)
-    # # th_distance.start()
-    # th_luminosite = communication.Communication(
-    #     arduino, 5, "luminositetest", "pourcent", consts.READ_LUMINOSITE, mutex)
-    # th_luminosite.start()
 
-    # liste_th.append(th_distance)
-    # liste_th.append(th_luminosite)
-
-
+# page d'accueil : avec la liste des données mesurées
 @app.route('/')
 def index():
-    return render_template('index.html', liste=liste_th, capteurs_libre=capteurs_libre)
+    return render_template('index.html', liste=threads_en_cours, capteurs_libre=capteurs_libre)
 
-
-@app.route('/result', methods=['POST', 'GET'])
-def result():
-   if request.method == 'POST':
-      result = request.form
-      return render_template("result.html", result=result)
-
-
+# page pour modifier les paramètres d'un capteur
 @app.route("/modifier/<requete_arduino>")
 def modifier(requete_arduino):
-    for thread in liste_th:
+    for thread in threads_en_cours:
         if thread.requete == requete_arduino:
             return render_template("modification.html", capteur=thread)
     return "oh nn"
 
-
+# page appelée pour supprimer une acquisition de données
 @app.route("/supprimer/<requete_arduino>")
 def supprimer(requete_arduino):
-    for thread in liste_th:
+    for thread in threads_en_cours:
         if thread.requete == requete_arduino:
             thread.cancel()
             for capteur in consts.CAPTEURS:
                 print(capteur)
                 if capteur["requete"] == requete_arduino:
                     capteurs_libre.append(capteur)
-            liste_th.remove(thread)
+            threads_en_cours.remove(thread)
             return render_template("ok.html")
     return "oh nn"
 
-
+# page pour ajouter une acquisition de données
 @app.route("/ajout")
 def ajout():
     return render_template("ajout.html", capteurs_libre=capteurs_libre)
 
-
+#page appelée qui ajoute l'acquisition de données
 @app.route("/ajouter", methods=["POST", "GET"])
 def ajouter():
     resultat = request.form
@@ -101,14 +88,14 @@ def ajouter():
                 resultat["frequence"]), capteur, resultat["nom"], mutex)
             capteurs_libre.remove(capteur)
             thread.start()
-            liste_th.append(thread)
+            threads_en_cours.append(thread)
     return render_template("ok.html")
 
-
+# page appelée qui enregistre les modifications
 @app.route("/enregistrer", methods=["POST", "GET"])
 def enregistrer():
     resultat = request.form
-    for thread in liste_th:
+    for thread in threads_en_cours:
         if thread.requete == resultat["requete"]:
             thread.cancel()
             requete = resultat["requete"]
@@ -116,13 +103,14 @@ def enregistrer():
                 if capteur["requete"] == requete:
                     nouveau_thread = communication.Communication(thread.port_serie, float(
                         resultat["frequence"]), capteur, resultat["nom-donnee"], thread.mutex)
-            liste_th.remove(thread)
-            liste_th.append(nouveau_thread)
+            threads_en_cours.remove(thread)
+            threads_en_cours.append(nouveau_thread)
             nouveau_thread.start()
-            print(liste_th)
+            print(threads_en_cours)
     return render_template("ok.html")
 
 
+# fonction pour démarrer l'acquisition des données avec l'arduino
 def start_runner():
     def start_loop():
         demarre = False
